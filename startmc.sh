ff1ff65fa307512c62c158f2d41f88a644c25171
u9g <git@u9g.dev>
2022-03-13 14:57:06 -0400

    Create startmc.sh

---
+++ startmc.sh
@@ -0,0 +1,7 @@
+curl -sL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y wget apt-transport-https
+wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /usr/share/keyrings/adoptium.asc
+echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
+sudo apt-get update # update if you haven't already
+sudo apt-get install temurin-17-jdk
