60218baf181dcf4297c24583009ae3873a6c7497
u9g <git@u9g.dev>
2022-03-13 14:58:53 -0400

    Update startmc.sh

--- startmc.sh
+++ startmc.sh
@@ -3,5 +3,5 @@ sudo apt-get install -y nodejs
 sudo apt-get install -y wget apt-transport-https
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /usr/share/keyrings/adoptium.asc
 echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
-sudo apt-get update # update if you haven't already
-sudo apt-get install temurin-17-jdk
+sudo apt-get update -y
+sudo apt-get install temurin-17-jdk -y
