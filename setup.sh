9a86121093f2186ba70a916de78570be75dd719e
u9g <git@u9g.dev>
2022-03-13 21:38:29 -0400

    Update and rename startmc.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium
 sudo apt-get update -y
 sudo apt-get install temurin-17-jdk -y
 npm i -g minecraft-wrap
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
